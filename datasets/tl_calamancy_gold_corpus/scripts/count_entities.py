eb8482f3dd4ab05d068b62efebed880ac649d8a7
Lj Miranda <ljvmiranda@gmail.com>
2023-01-07 19:57:24 +0800

    Add script for counting entities

---
+++ datasets/tl_calamancy_gold_corpus/scripts/count_entities.py
@@ -0,0 +1,37 @@
+from collections import Counter
+from pathlib import Path
+
+import spacy
+import typer
+from spacy.tokens import DocBin
+from wasabi import msg
+
+Arg = typer.Argument
+Opt = typer.Option
+
+
+def count_entities(
+    input_path: Path = typer.Argument(..., help="Path to the spaCy file."),
+    lang: str = Opt("tl", "--lang", "-l", help="Language code for Tagalog."),
+):
+    nlp = spacy.blank(lang)
+    doc_bin = DocBin().from_disk(input_path)
+    docs = doc_bin.get_docs(nlp.vocab)
+
+    counter = Counter()
+    for doc in docs:
+        for ent in doc.ents:
+            if ent.label_ not in counter.keys():
+                counter[ent.label_] = 1
+            else:
+                counter[ent.label_] += 1
+
+    msg.info(f"Found {len(doc_bin)} documents: {_format_counts(counter)}")
+
+
+def _format_counts(counter: Counter) -> str:
+    return ", ".join([f"{k} ({v})" for k, v in counter.items()])
+
+
+if __name__ == "__main__":
+    typer.run(count_entities)
