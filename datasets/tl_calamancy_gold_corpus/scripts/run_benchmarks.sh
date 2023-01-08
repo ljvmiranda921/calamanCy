a759354e11d5526ce987b619446637b7d7fd9baf
Lj Miranda <ljvmiranda@gmail.com>
2023-01-08 21:18:42 +0800

    Update pretrained weights file

--- datasets/tl_calamancy_gold_corpus/scripts/run_benchmarks.sh
+++ datasets/tl_calamancy_gold_corpus/scripts/run_benchmarks.sh
@@ -1,5 +1,5 @@
 # Run experiments for word vectors
 python3 -m scripts.benchmark supervised_nsv_npt --num-trials 3 --gpu-id 0
 python3 -m scripts.benchmark supervised_ysv_npt --num-trials 3 --vectors vectors/fasttext-tl --gpu-id 0 
-python3 -m scripts.benchmark supervised_nsv_ypt --num-trials 3 --init-tok2vec assets/tl_tlunified_pt.bin --gpu-id 0 
-python3 -m scripts.benchmark supervised_ysv_ypt --num-trials 3 --vectors vectors/fasttext-tl --init-tok2vec assets/tl_tlunified_pt.bin --gpu-id 0 
+python3 -m scripts.benchmark supervised_nsv_ypt --num-trials 3 --init-tok2vec assets/tl_tlunified_pt_e10.bin --gpu-id 0 
+python3 -m scripts.benchmark supervised_ysv_ypt --num-trials 3 --vectors vectors/fasttext-tl --init-tok2vec assets/tl_tlunified_pt_e10.bin --gpu-id 0 
