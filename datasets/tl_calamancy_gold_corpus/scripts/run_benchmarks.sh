e2993440875586d9a0e19b13ad3f0b5a639ed659
Lj Miranda <ljvmiranda@gmail.com>
2023-01-09 20:53:01 +0800

    Update benchmark experiments to include ner_vects

--- datasets/tl_calamancy_gold_corpus/scripts/run_benchmarks.sh
+++ datasets/tl_calamancy_gold_corpus/scripts/run_benchmarks.sh
@@ -1,5 +1,8 @@
-# Run experiments for word vectors
+# Word vector experiments
+## Main results
 python3 -m scripts.benchmark supervised_nsv_npt --num-trials 3 --gpu-id 0
 python3 -m scripts.benchmark supervised_ysv_npt --num-trials 3 --vectors vectors/fasttext-tl --gpu-id 0 
-python3 -m scripts.benchmark supervised_nsv_ypt --num-trials 3 --init-tok2vec assets/tl_tlunified_pt.bin --gpu-id 0 
-python3 -m scripts.benchmark supervised_ysv_ypt --num-trials 3 --vectors vectors/fasttext-tl --init-tok2vec assets/tl_tlunified_pt.bin --gpu-id 0 
+python3 -m scripts.benchmark supervised_nsv_ypt --num-trials 3 --init-tok2vec assets/tl_tlunified_pt_chars.bin --gpu-id 0 
+python3 -m scripts.benchmark supervised_ysv_ypt --num-trials 3 --vectors vectors/fasttext-tl --init-tok2vec assets/tl_tlunified_pt_chars.bin --gpu-id 0 
+## Pretrain characters vs pretrain vectors
+python3 -m scripts.benchmark supervised_nsv_ypt_vects --num-trials 3 --config ner_vects.cfg --init-tok2vec assets/tl_tlunified_pt_chars.bin --gpu-id 0 
