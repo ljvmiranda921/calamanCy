1bc06c34baa86faab08e3902c53e057550be996c
Lj Miranda <ljvmiranda@gmail.com>
2023-01-11 18:04:57 +0800

    Add vector benchmarking script

--- datasets/tl_calamancy_gold_corpus/scripts/run_benchmarks.sh
+++ datasets/tl_calamancy_gold_corpus/scripts/run_benchmarks.sh
@@ -6,3 +6,6 @@ python3 -m scripts.benchmark supervised_nsv_ypt --num-trials 3 --init-tok2vec as
 python3 -m scripts.benchmark supervised_ysv_ypt --num-trials 3 --vectors vectors/fasttext-tl --init-tok2vec assets/tl_tlunified_pt_chars.bin --gpu-id 0 
 ## Pretrain characters vs pretrain vectors
 python3 -m scripts.benchmark supervised_nsv_ypt_vects --num-trials 3 --config ner_vects.cfg --init-tok2vec assets/tl_tlunified_pt_chars.bin --gpu-id 0 
+## fastText, fastText TLUnified, floret TLUnified 
+python3 -m scripts.benchmark supervised_ysv_npt_fasttext_tlunified --num-trials 3 --vectors vectors/fasttext-tl-tlunified --gpu-id 0 
+python3 -m scripts.benchmark supervised_ysv_npt_floret_tlunified --num-trials 3 --vectors vectors/floret-tl-tlunified --gpu-id 0 
