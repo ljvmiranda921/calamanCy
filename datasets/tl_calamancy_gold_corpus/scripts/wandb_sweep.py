213be041509d804c7622ee8b5ba671d51677558c
Lj Miranda <ljvmiranda@gmail.com>
2023-02-13 23:29:31 +0800

    Change style of running sweep
    I want to run the sweep by code but I want to provide the sweep config
    from an external YAMl file.

--- datasets/tl_calamancy_gold_corpus/scripts/wandb_sweep.py
+++ datasets/tl_calamancy_gold_corpus/scripts/wandb_sweep.py
@@ -7,6 +7,7 @@ from spacy import util
 from spacy.cli._util import parse_config_overrides
 from spacy.training.initialize import init_nlp
 from spacy.training.loop import train
+from srsly import read_yaml
 from thinc.api import Config
 
 Arg = typer.Argument
@@ -25,20 +26,28 @@ def wandb_sweep(
     ctx: typer.Context,
     default_config: Path = typer.Argument(..., help="Path to the spaCy training configuration."), 
     wandb_config: Path = typer.Option(DEFAULT_WANDB_CONFIG, "--wandb-config", "--config", "-C", help="Path to the WandB YAML configuration file."),
+    num_trials: int = typer.Option(10, "--num-trials", "-n", help="Number of trials to run the each hyperparameter combination."),
+    project_name: str = typer.Option("calamanCy", help="Project name to save the sweep results."),
     output_path: Optional[Path] = typer.Option(None, "--output-path", "-o", help="Path to store the trained models."),
     gpu_id: int = Opt(0, "--gpu-id", "-G", help="Set the GPU ID. Use -1 for CPU."),
     # fmt: on
 ):
     """Perform a WandB sweep given a sweep config and a spaCy configuration"""
-    overrides = parse_config_overrides(ctx.args)
-    loaded_local_config = util.load_config(default_config, overrides=overrides)
-    with wandb.init(config=str(wandb_config)) as run:
-        sweeps_config = Config(util.dot_to_dict(run.config))
-        merged_config = Config(loaded_local_config).merge(sweeps_config)
-        nlp = init_nlp(merged_config, use_gpu=gpu_id)
-        if output_path:
-            output_path.mkdir(parents=True, exist_ok=True)
-        train(nlp, output_path=output_path, use_gpu=gpu_id)
+
+    def train_spacy():
+        overrides = parse_config_overrides(ctx.args)
+        loaded_local_config = util.load_config(default_config, overrides=overrides)
+        with wandb.init(project=project_name) as run:
+            sweeps_config = Config(util.dot_to_dict(run.config))
+            merged_config = Config(loaded_local_config).merge(sweeps_config)
+            nlp = init_nlp(merged_config, use_gpu=gpu_id)
+            if output_path:
+                output_path.mkdir(parents=True, exist_ok=True)
+            train(nlp, output_path=output_path, use_gpu=gpu_id)
+
+    sweep_config = dict(read_yaml(wandb_config))
+    sweep_id = wandb.sweep(sweep_config, project=project_name)
+    wandb.agent(sweep_id, train_spacy, count=num_trials)
 
 
 if __name__ == "__main__":
