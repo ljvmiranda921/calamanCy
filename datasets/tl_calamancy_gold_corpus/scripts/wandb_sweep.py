fdfc837ae4d077adac702452ef5383b9df3b362f
Lj Miranda <ljvmiranda@gmail.com>
2023-02-13 20:30:37 +0800

    Allow overrides for wandb script
    This commit allows us to add overrides to the configuration file via the
    CLI.

--- datasets/tl_calamancy_gold_corpus/scripts/wandb_sweep.py
+++ datasets/tl_calamancy_gold_corpus/scripts/wandb_sweep.py
@@ -4,6 +4,7 @@ from typing import Optional
 import typer
 import wandb
 from spacy import util
+from spacy.cli._util import parse_config_overrides
 from spacy.training.initialize import init_nlp
 from spacy.training.loop import train
 from thinc.api import Config
@@ -13,9 +14,15 @@ Opt = typer.Option
 
 DEFAULT_WANDB_CONFIG = Path().parent / "configs" / "sweeps" / "sweep.yml"
 
+app = typer.Typer()
 
+
+@app.command(
+    context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
+)
 def wandb_sweep(
     # fmt: off
+    ctx: typer.Context,
     default_config: Path = typer.Argument(..., help="Path to the spaCy training configuration."), 
     wandb_config: Path = typer.Option(DEFAULT_WANDB_CONFIG, "--wandb-config", "--config", "-C", help="Path to the WandB YAML configuration file."),
     output_path: Optional[Path] = typer.Option(None, "--output-path", "-o", help="Path to store the trained models."),
@@ -23,15 +30,16 @@ def wandb_sweep(
     # fmt: on
 ):
     """Perform a WandB sweep given a sweep config and a spaCy configuration"""
-    loaded_local_config = util.load_config(default_config)
+    overrides = parse_config_overrides(ctx.args)
+    loaded_local_config = util.load_config(default_config, overrides=overrides)
     with wandb.init(config=str(wandb_config)) as run:
         sweeps_config = Config(util.dot_to_dict(run.config))
         merged_config = Config(loaded_local_config).merge(sweeps_config)
-        nlp = init_nlp(merged_config)
+        nlp = init_nlp(merged_config, use_gpu=gpu_id)
         if output_path:
             output_path.mkdir(parents=True, exist_ok=True)
         train(nlp, output_path=output_path, use_gpu=gpu_id)
 
 
 if __name__ == "__main__":
-    typer.run(wandb_sweep)
+    app()
