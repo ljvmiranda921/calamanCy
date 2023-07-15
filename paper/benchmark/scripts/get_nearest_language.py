f74a8548c31b6287b649928353b2405b3f7cff6f
Lj Miranda <ljvmiranda@gmail.com>
2023-07-15 21:55:49 +0800

    [wip] Update

---
+++ paper/benchmark/scripts/get_nearest_language.py
@@ -0,0 +1,20 @@
+"""Get nearest language given a language code using WALS
+
+This script attempts to give you the nearest language for a given target using
+the typological features found in WALS. A lot of these methods are based from
+the work, Cross-lingual parser selection for low-resource languages, by Zeljko
+Agic.
+
+The code was adapted from the original repository:
+https://github.com/zeljkoagic/freasy
+"""
+
+import typer
+
+
+def get_nearest_language():
+    pass
+
+
+if __name__ == "__main__":
+    typer.run(get_nearest_language)
