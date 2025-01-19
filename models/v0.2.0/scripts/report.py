87319cdbe2f548704f5c3a9c543104bb62775950
ljvmiranda921 <ljvmiranda@gmail.com>
2025-01-19 07:15:40 -0800

    wip

--- models/v0.2.0/scripts/report.py
+++ models/v0.2.0/scripts/report.py
@@ -1,11 +1,21 @@
 from pathlib import Path
 
 import typer
+import pandas as pd
 from srsly import read_jsonl
 from wasabi import msg
 
 
-def report():
+def report(
+    # fmt: off
+    indir: Path = typer.Argument(..., help="")
+    # fmt: on
+):
+    """Return a table of evaluation results
+
+    The input to `indir` must be a directory where the first-level directories are the model names,
+    with JSON files from `spacy evaluate` in this file format: {task}_{dataset}.json
+    """
     pass
 
 
